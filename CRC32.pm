8979151ec0292a7ff44a2e04a9561b275158b4c1
Lee Johnson <lee@givengain.ch>
2017-05-22 08:43:00 +0200

    "initial" commit of String::CRC32 code
    "initial" in that i may redo this is Sönke provides me with an original
    repo that i can rebuild the history with
    
    this initial commit fixes a few dist Kwalitee issues and "this is a high
    river" module issues:
    
        * Add Changes file
        * Add link to github repo
        * Add strict and warnings
        * Add LICENSE to POD + LICENSE file
        * Add META.* files through make dist
        * Add .travis.yml for CI
    
    a _01 DEV release will be uploaded to CPAN shortly

---
+++ CRC32.pm
@@ -0,0 +1,24 @@
+
+package String::CRC32;
+
+use strict;
+use warnings;
+
+require Exporter;
+require DynaLoader;
+
+use vars qw/ @ISA $VERSION @EXPORT_OK @EXPORT /;
+
+@ISA = qw(Exporter DynaLoader);
+
+$VERSION = 1.5_01;
+
+# Items to export into callers namespace by default
+@EXPORT = qw(crc32);
+
+# Other items we are prepared to export if requested
+@EXPORT_OK = qw();
+
+bootstrap String::CRC32;
+
+1;
